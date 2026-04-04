e93f61a6a0c85d244483695c3bb6ec1c21692929
tonyhan18 <ourclub7279@gmail.com>
2026-04-04 14:28:04 +0900

    FEAT : quartz 페이지 업데이트

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Bongsun's Dev Log",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
